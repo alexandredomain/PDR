f9ba0f6e1f024cc660a473a7f2e881dad13929da
Alexandre Domain <domain.alexandre@gmail.com>
2017-03-21 17:00:03 +0100

    ++

--- Code/main.c
+++ Code/main.c
@@ -11,24 +11,23 @@
 int main(int pintArgc, char * ptstrArgv[]) {
 
     printf("*************** Traitement du fichier *.xls en format texte ***************\n");
-    XLStoCSV("../Relevés/GCE1_Elec.xls");
+    XLStoCSV("../Relevés/SB_CO2.xls");
     removeEmptyLinesCSV();
     printf("***************************************************************************\n\n");
 
 
-    printf("************** Insertion des données dans la base de données **************\n");
 
+    printf("************** Insertion des données dans la base de données **************\n");
     sqlite3 *db; // bdd
 
     versionSQLite();
     openBDD(&db);
     createTableBatiment(db);
-    createTableBatiment(db);
 
     // Requête factice avec prise en charge de la date
     // SELECT date('1899-12-29', '+42737 day');
     char *requete = NULL;
-    asprintf(&requete, "INSERT INTO batiment (nom, surface, date_jour) VALUES ('%s', %d, date('1899-12-30', '+%i day'));", "Tom", 20, 42736);
+    asprintf(&requete, "INSERT INTO batiment (nom, surface, date_jour) VALUES ('%s', %d, date('1899-12-30', '+%i day'));", "Tom", 20, 42644);
     int codeRetour = 0;
     char *feedbackErrorSQL = NULL;
 
